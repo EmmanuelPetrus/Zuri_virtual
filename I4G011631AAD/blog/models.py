f9d72d52e296e1d5cf573ca6a5f83b736ce80237
Emmanuel Peter <peterojonilemi@gmail.com>
2022-06-16 22:32:20 +0100

    Successfully created the models

---
+++ I4G011631AAD/blog/models.py
@@ -0,0 +1,11 @@
+from xml.parsers.expat import model
+from django.db import models
+from django.contrib.auth import get_user_model
+# Create your models here.
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField(max_length=200)
+    Author = get_user_model()
+    Created_date = models.DateTimeField()
+    
+
